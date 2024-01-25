74a1c8f5d223369c5cb0e1a50b8cefb818f2806b
Diogo-MD <diogomdossantos@gmail.com>
2024-01-24 22:15:54 -0300

    Atualização Aula 24/01/24 (Professor: Aécio Brito)

--- aulas/aula09/script.js
+++ aulas/aula09/script.js
@@ -21,18 +21,42 @@ class conta {
             return false; // Digite um valor válido
         }
     }
+
+    toString() {
+        return `Número: ${this.numero} - Saldo: ${this.saldo}`
+    }
 }
 
 class contaCorrente extends conta {
-    constructor(numero, saldo, limiteChequeEspecial) {
+    constructor(numero, saldo = 0, limiteChequeEspecial = 200) {
         super(numero, saldo);
         this.limiteChequeEspecial = limiteChequeEspecial
     }
+
+    sacar (valor) {
+        if (this.saldo >= valor + this.limiteChequeEspecial && valor > 0) {
+            this.saldo - (valor + this.limiteChequeEspecial)
+            return true; // Saque com valor de cheque especial feito com sucesso
+        } else {
+            return false; // Saldo insuficiente ou valor inválido
+        }
+    }
+
+    
 }
 
 class contaPoupanca extends conta {
-    constructor(numero, saldo, taxaRendimento) {
-        super(numero, saldo)
-        this.taxaRendimento = taxaRendimento
+    constructor(numero, saldo = 0, taxaRendimento = 0.02) {
+        super(numero, saldo);
+        this.taxaRendimento = taxaRendimento;
     }
 }
+
+let contaCorr = new contaCorrente(1122);
+let contaPoup = new contaPoupanca(2345);
+console.log(contaCorr.toString());
+console.log(contaPoup.toString());
+
+console.log("Saque de 500 reais em cada")
+contaCorr.sacar(200)
+contapoup.sacar(200)
