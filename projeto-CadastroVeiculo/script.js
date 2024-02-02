fb07eeac8403af551df6797f04163aa02e4ab6c7
Diogo-MD <diogomdossantos@gmail.com>
2024-02-01 21:26:29 -0300

    Atualização Aula 01/02/24 (Professor: Aécio Brito

--- projeto-CadastroVeiculo/script.js
+++ projeto-CadastroVeiculo/script.js
@@ -5,6 +5,9 @@
     - Adicione um metodo calcularDistanciaMaxia, que retornará a autonomia * capacidadeTanque
     - Adicione um metodo exibirDetalhes, que retornará os dados concatenados de:
         * marca, modelo, cor, preco.toFixed(2)
+
+    #Parte 2:
+        - Crie uma função cadastrarVeiculo
 */
 
 class veiculo {
@@ -30,8 +33,23 @@ class veiculo {
 
 const veiculos = [];
 
-veiculos.push(new veiculo("Fiat", "Uno", 60000, "Branco", 16,90,"www.url.com"));
-veiculos.push(new veiculo("fiat", "palio", 90000, "vermelho", 14, 90, "www.url88.com"));
-veiculos.push(new veiculo("chrevrolet", "cruze", 120000, "amarelo", 11, 90, "www.url.com"));
+// Função para cadastrar veículo
+function cadastrarVeiculo() {
+    // Recebimento de valores do HTML
+    const marca = document.getElementById("marca").value;
+    const modelo = document.getElementById("modelo").value;
+    const preco = document.getElementById("preco").value;
+    const cor = document.getElementById("cor").value;
+    const autonomia = document.getElementById("autonomia").value;
+    const capacidadeTanque = document.getElementById("capacidadeTanque").value;
+    const imagemURL = document.getElementById("imagem").value;
 
-console.log(veiculos)
+    // Instanciar a um novo objeto, passando os valores pedidos no construtor
+    const Veiculo = new veiculo();
+}
+
+// veiculos.push(new veiculo("Fiat", "Uno", 60000, "Branco", 16,90,"www.url.com"));
+// veiculos.push(new veiculo("fiat", "palio", 90000, "vermelho", 14, 90, "www.url88.com"));
+// veiculos.push(new veiculo("chrevrolet", "cruze", 120000, "amarelo", 11, 90, "www.url.com"));
+
+// console.log(veiculos)
