f0208db3df88ff01d4532aa99bc952e58578d453
Diogo-MD <diogomdossantos@gmail.com>
2024-02-02 21:56:07 -0300

    Atualização Aula 02/02/24 (Professor: Aécio Brito)

--- projeto-CadastroVeiculo/script.js
+++ projeto-CadastroVeiculo/script.js
@@ -11,17 +11,18 @@
 
 #Exercício:
     - Adicione ao veículo um anoDeFabricacao
-    - Adicione uma função que receba como parametro um ano qualquer, e retorne se 
-    o veículo é isento de IPVA naquele ano.
+    - Adicione uma função que retorne se o veículo é isento de IPVA.
+    - Utilize o Date().getFullYear();
 */
 class Veiculo {
-    constructor(marca, modelo, preco, cor, autonomia, capacidadeTanque, imagemURL) {
+    constructor(marca, modelo, preco, cor, autonomia, capacidadeTanque, anoDeFabricacao, imagemURL) {
         this.marca = marca;
         this.modelo = modelo;
         this.preco = preco;
         this.cor = cor;
         this.autonomia = autonomia;
         this.capacidadeTanque = capacidadeTanque;
+        this.anoDeFabricacao = anoDeFabricacao;
         this.imagemURL = imagemURL;
     }
 
@@ -31,6 +32,13 @@ class Veiculo {
 
     exibirDetalhes() {
         return `${this.marca} ${this.modelo} - ${this.cor} - R$ ${this.preco.toFixed(2)}`;
+    }
+
+    ehIsentoIPVA(){
+        const dataAtual = new Date;
+        const anoAtual = dataAtual.getFullYear();
+
+        return anoAtual - this.anoDeFabricacao >= 20;
     }
 }
 
@@ -45,11 +53,13 @@ function cadastrarVeiculo() {
     const cor = document.getElementById("cor").value;
     const autonomia = parseInt(document.getElementById("autonomia").value);
     const capacidadeTanque = parseInt(document.getElementById("capacidadeTanque").value);
+    const anoDeFabricacao = parseInt(document.getElementById("anoDeFabricacao").value);
     const imagemURL = document.getElementById("imagemURL").value;
 
     // Instanciar um novo objeto veículo, passando os valores pedidos no construtor
-    const veiculo = new Veiculo(marca, modelo, preco, cor, autonomia, capacidadeTanque, imagemURL);
-
+    const veiculo = new Veiculo(marca, modelo, preco, cor, autonomia, capacidadeTanque, anoDeFabricacao, imagemURL);
+    console.log(veiculo.ehIsentoIPVA());
+  
     // Adicionar o veículo a nossa lista "banco de dados"
     veiculos.push(veiculo);
 
