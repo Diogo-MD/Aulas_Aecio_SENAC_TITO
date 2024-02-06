edc125af50004d349f2463a3944d48491313de8e
Diogo-MD <diogomdossantos@gmail.com>
2024-02-06 20:15:12 -0300

    Crianção Estrutura HTML

--- projeto-ContaBancaria/script.js
+++ projeto-ContaBancaria/script.js
@@ -29,6 +29,7 @@ class Conta {
         retornar true em caso de sucesso, false caso não
         */
         if (valorSaque <= this.saldo && valorSaque > 0) {
+            this.saldo -= valorSaque;
             console.log();
             return true;
         }
@@ -86,19 +87,19 @@ class ContaPoupanca extends Conta {
 let contas = [];
 let clientes = [];
 
-let clienteA = new Cliente("Fulano", "1234567890");
-clientes.push(clienteA);
+// let clienteA = new Cliente("Fulano", "1234567890");
+// clientes.push(clienteA);
 
-let clienteB = new Cliente("Beltrano", "0987654321");
-clientes.push(clienteB);
+// let clienteB = new Cliente("Beltrano", "0987654321");
+// clientes.push(clienteB);
 
-let contaX = new ContaCorrente(clienteA, 123, 100, 150);
-contas.push(contaX);
+// let contaX = new ContaCorrente(clienteA, 123, 100, 150);
+// contas.push(contaX);
 
-let contaY = new ContaPoupanca(clienteB, 111, 100, 0.01);
-contas.push(contaY);
+// let contaY = new ContaPoupanca(clienteB, 111, 100, 0.01);
+// contas.push(contaY);
 
-contaY.transferir(50, contaX);
+// contaY.transferir(50, contaX);
 
-console.log("Conta Y: ", contaY);
-console.log("Conta X: ", contaX);
+// console.log("Conta Y: ", contaY);
+// console.log("Conta X: ", contaX);
