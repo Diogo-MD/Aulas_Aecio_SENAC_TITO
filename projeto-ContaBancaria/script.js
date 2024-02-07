6cf46ac21010907ed5bf71e6fd052eecf4360f29
Diogo-MD <diogomdossantos@gmail.com>
2024-02-06 21:54:26 -0300

    Atualização Aula 06/02/23

--- projeto-ContaBancaria/script.js
+++ projeto-ContaBancaria/script.js
@@ -51,6 +51,10 @@ class Conta {
             return true;
         }
     }
+
+    toString() {
+        return `Numero: ${this.numero} - Saldo: ${this.saldo} - Cliente: ${clie}`
+    }
 }
 
 class ContaCorrente extends Conta {
@@ -99,14 +103,30 @@ function cadastrarCliente() {
     clientes.push(cliente);
 
     atualizarSeletorClientes()
+    exibirClientes();
+    
+    document.getElementById("clienteForm").reset();
 }
 
-function atualizarSeletorClientes(){
+// Exibir clientes cadastrados
+function exibirClientes() {
+    const clientesList = document.getElementById("clientesList");
+    // Limpar a lista antes de exibir os clientes
+    clientesList.innerHTML = "";
+
+    for (let i = 0; i < clientes.length; i++) {
+        const clienteItem = document.createElement("li");
+        clienteItem.textContent = `Nome: ${clientes[i].nome} - CPF: ${clientes[i].cpf}`;
+        clientesList.appendChild(clienteItem);
+    }
+}
+
+function atualizarSeletorClientes() {
     const seletorClientes = document.getElementById("cliente")
 
     seletorClientes.innerHTML = ""
 
-    clientes.forEach(cliente =>{
+    clientes.forEach(cliente => {
         const option = document.createElement("option");
         option.value = cliente.cpf;
         option.textContent = cliente.nome;
@@ -144,4 +164,32 @@ function cadastrarConta() {
 
     contas.push(conta);
 
+    exibirContas();
+
+    document.getElementById("contaForm").reset();
 }
+
+function exibirContas() {
+    const contasList = document.getElementById("contasList");
+    // Limpar a lista antes de exibir as contas
+    contasList.innerHTML = "";
+
+    for (let i = 0; i < contas.length; i++) {
+        const contaItem = document.createElement("li");
+        const contaCard = criarContaCard(contas[i]);
+        contasList.appendChild(contaCard);
+        contasList.appendChild(contaItem);
+    }
+}
+
+function criarContaCard(conta) {
+    const contaCard = document.createElement("div");
+    contaCard.className = "conta-card";
+
+    const detalhesConta = document.createElement("div");
+    detalhesConta.textContent = conta.toString();
+    contaCard.appendChild(detalhesConta);
+
+    return contaCard;
+}
+
